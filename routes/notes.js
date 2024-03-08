24caf88a0bcc8c22abef58c8af142991ef2c050f
Ian Hart <ianhart7790@gmail.com>
2024-03-08 12:28:26 -0500

    feat: a little refactor and add api route for deleting a note

--- routes/notes.js
+++ routes/notes.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const notes = require('express').Router();
+const databaseFileName = path.join(__dirname, '../db/db.json');
 
 // read the db file and send the parsed json back to the client
 notes.get('/', (req, res) => {
-  fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+  fs.readFile(databaseFileName, (err, data) => {
     if (err) {
       return res.status(500).json({ error: 'Internal Server Error', message: 'Unable to read read from database' });
     }
@@ -32,7 +33,26 @@ notes.post('/', (req, res) => {
         return res.status(500).json({ error: 'Internal Server Error', message: 'Unable to write to database' });
       }
 
-      res.json(newNote);
+      res.status(201).json(newNote);
+    });
+  });
+});
+
+notes.delete('/:id', (req, res) => {
+  fs.readFile(databaseFileName, (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal Server Error', message: 'Unable to read read from database' });
+    }
+    let notes = JSON.parse(data);
+
+    notes = notes.filter((note) => note.id !== req.params.id);
+
+    fs.writeFile(databaseFileName, JSON.stringify(notes), (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'Internal Server Error', message: 'Unable to write to database' });
+      }
+
+      res.status(200).json({ message: 'success' });
     });
   });
 });
