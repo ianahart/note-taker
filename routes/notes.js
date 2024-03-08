ae0e4763d9775ffcf7a41fb73afbfa18a1c2cceb
Ian Hart <ianhart7790@gmail.com>
2024-03-08 12:17:01 -0500

    feat: add api route for creating a new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const notes = require('express').Router();
 
@@ -9,6 +10,30 @@ notes.get('/', (req, res) => {
       return res.status(500).json({ error: 'Internal Server Error', message: 'Unable to read read from database' });
     }
     res.status(200).json(JSON.parse(data));
+  });
+});
+
+// create a new note and add it to existing notes from the db file
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
+  const newNote = { id: uuidv4(), title, text };
+  const databaseFileName = path.join(__dirname, '../db/db.json');
+
+  fs.readFile(databaseFileName, (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal Server Error', message: 'Unable to read read from database' });
+    }
+    let notes = JSON.parse(data);
+
+    notes = [...notes, newNote];
+
+    fs.writeFile(databaseFileName, JSON.stringify(notes), (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'Internal Server Error', message: 'Unable to write to database' });
+      }
+
+      res.json(newNote);
+    });
   });
 });
 
